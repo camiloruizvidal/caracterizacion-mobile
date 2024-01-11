ac2f41aad50a45997596249f2395a3ba696f7c9f
camiloruiz <camiloruizvidal@gmail.com>
2024-01-11 17:38:50 -0500

    Delete onOnIninit

--- src/app/modules/formgenerator/inputs/numbers/numbers.component.ts
+++ src/app/modules/formgenerator/inputs/numbers/numbers.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ISteperValues } from '../../interfaces/interface';
 import { BaseInputComponent } from '../base-input/base-input.component';
 
--- src/app/modules/formgenerator/steper/steper.component.ts
+++ src/app/modules/formgenerator/steper/steper.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ESteperType, IFamilyCard, IStepers } from '../interfaces/interface';
 
 @Component({
